f19e5b687a372b7497bed1d0c902f1a99592c385
Felipe Pizarro Márquez <fpizarro92@live.cl>
2025-01-13 11:49:22 -0300

    Endpoint actualizar appversion

--- app/api/mobile/version/route.js
+++ app/api/mobile/version/route.js
@@ -6,5 +6,14 @@ export async function GET(request) {
   const headersList = headers();
   const operatorId = headersList.get("operatorId");
   const operator = await Operator.findByPk(operatorId);
-  return NextResponse.json(operator?.appVersion);
+  return NextResponse.json({ appVersion: operator?.appVersion });
+}
+
+export async function PUT(request) {
+  const headersList = headers();
+  const operatorId = headersList.get("operatorId");
+  const { appVersion } = await request.json();
+  const operator = await Operator.findByPk(operatorId);
+  const result = await operator.update({ appVersion });
+  return NextResponse.json({ appVersion: result?.appVersion });
 }
